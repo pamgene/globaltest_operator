a03e45b3600e4a937d843b201b4805ed2c766b20
lucaslelann <lucas.lelann@gmail.com>
2022-02-21 10:44:26 +0000

    other(workspace)

--- workspace.R
+++ workspace.R
@@ -1,10 +1,83 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
+library(globaltest)
 
-options("tercen.workflowId" = "3a85f3a46cd4009b31373508220064f2")
-options("tercen.stepId"     = "88d3b0b5-b876-4f9c-830f-6ed31bdecfce")
+options("tercen.workflowId" = "74748d130d4db6ff075047cf7e02c025")
+options("tercen.stepId"     = "38717db4-7a32-4e08-aa3c-bc5821bab110")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-source("./main.R")
+aCtx = tercenCtx()
+ctx = tercenCtx()
+
+stdz <- FALSE
+if(!is.null(aCtx$op.value("standardize"))) stdz <- as.logical(aCtx$op.value("standardize"))
+
+directional <- FALSE
+if(!is.null(aCtx$op.value("directional"))) directional <- as.logical(aCtx$op.value("directional"))
+
+modeltype = "auto"
+if(!is.null(aCtx$op.value("modeltype"))) modeltype <- aCtx$op.value("modeltype")
+
+getDataFrame = function(ctx){
+  if(!ctx$hasXAxis) stop("Define variables using an x-axis in Tercen")
+  if(length(ctx$colors) != 1) stop("Define grouping / response using a single variable as color in Tercen")
+  if(length(ctx$colors) != 1) stop("Define observations using a single variable as lable in Tercen")
+  df = ctx %>% 
+    select( .ri,.ci, .x, .y)
+  df = df %>% 
+    bind_cols(ctx$select(ctx$colors))
+  df = df %>% 
+    bind_cols(ctx$select(ctx$labels))
+  colnames(df) = c(colnames(df)[1:4], "response", "obs")
+  return(df)
+}
+
+gtest = function(df){
+  grp.info = df %>%
+    distinct(obs, response)
+  
+  if(modeltype == "auto" & is.numeric(grp.info$response)){
+    modeltype = "linear"
+  } else {
+    grp.info = grp.info %>%
+      mutate(response = response %>% as.factor)
+    
+    if(length(levels(grp.info$response)) == 2){
+      modeltype = "logistic"
+    } else {
+      modeltype = "multinomial"
+    }
+  }
+  
+  aGt = df %>%
+    reshape2::dcast(obs ~ .x, value.var = ".y") %>%
+    left_join(grp.info, by = "obs") %>%
+    select(-obs) %>%
+    gt(response ~ . , data = ., model = modeltype, standardize = stdz, directional = directional)
+  
+  if(modeltype == "logistic"){
+    ddf = df %>%
+      group_by(response) %>%
+      summarize(grpm = mean(.y))
+    delta = ddf$grpm %>%
+      diff()
+  } else {
+    delta = NaN
+  }
+  
+  result = data.frame(p = p.value(aGt), globtest.statistic = aGt@result[2], nVariables = aGt@result[5], delta) 
+}
+
+result = aCtx %>%
+  getDataFrame() %>%
+  group_by(.ri, .ci) %>%
+  do(gtest(.)) %>%
+  ungroup() %>%
+  mutate(logp = -log10(p))
+
+result %>%
+  aCtx$addNamespace() %>%
+  aCtx$save()
+
